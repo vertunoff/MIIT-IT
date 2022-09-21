fc8e92cfb2928c56e283b12efc0865f57d267e61
vertunoff <83171047+vertunoff@users.noreply.github.com>
2022-09-21 22:34:03 +0300

    Add files via upload

---
+++ pr1.py
@@ -0,0 +1,4 @@
+x1, y1, x2, y2 = float(input()), float(input()), float(input()), float(input())
+a, b = abs(x2-x1), abs(y2-y1)
+print(f"{a*b:.2f}")
+print(f"{a+a+b+b:.2f}")
---
+++ pr3.py
@@ -0,0 +1,3 @@
+price = float(input()) * 7.61
+print(f"The final price is: {round(price*0.82, 2)}")
+print(f"The discount is: {round(price*0.18,2)} $.")
