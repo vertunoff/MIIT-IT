a5a76b6477ef8444df6d9c045ac5f42f005b9e1c
vertunoff <83171047+vertunoff@users.noreply.github.com>
2022-10-13 13:49:57 +0300

    Update 3.py

--- python_conditions/3.py
+++ python_conditions/3.py
@@ -1,6 +1,10 @@
 s = input()
 x = 12 if s == "Premiere" else 7.5 if s == "Normal" else 5 if s == "Discount" else None
 if x:
-    print(f"{x*int(input())*int(input()):.2f} $")
+    a, b = int(input()), int(input())
+    if a < 0 or b < 0:
+        print("error")
+    else:
+        print(f"{x*a*b:.2f} $")
 else:
     print("wrong type")
