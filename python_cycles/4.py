1854835824976262af01380531c8583ee470b464
vertunoff <83171047+vertunoff@users.noreply.github.com>
2022-10-20 14:18:40 +0300

    Update 4.py

--- python_cycles/4.py
+++ python_cycles/4.py
@@ -4,12 +4,15 @@ if not ((1<=a<=1000) and (1<=b<=1000)):
     quit()
 size = a * b
 while True:
-    n = input()
-    if n == 'STOP':
+    n = input().lower()
+    if n == 'stop':
         print(f'{size} pieces are left.')
         break
     n = int(n)
+    if n < 0:
+        print("error")
+        continue
     size -= n
-    if size < 0:
+    if size <= 0:
         print(f'No more cake left! You need {abs(size)} pieces more.')
         break
