26bf764e5d3d580ee6863b1f81928ce78930714c
vertunoff <83171047+vertunoff@users.noreply.github.com>
2022-11-02 15:07:56 +0300

    Add files via upload

--- prac4xjava/N4.java
+++ prac4xjava/N4.java
@@ -5,6 +5,7 @@ public class N4 {
         String fruit = scanner.nextLine().toLowerCase();
         String day = scanner.nextLine().toLowerCase();
         double kg = scanner.nextDouble();
+        double count=0;
         if (!((day.equals("monday")) || (day.equals("tuesday")) || (day.equals("wednesday")) || (day.equals("thursday")) || (day.equals("friday")) || (day.equals("saturday")) || (day.equals("sunday"))) || (kg < 0)) {
             System.out.println("error");
             return;
@@ -14,77 +15,51 @@ public class N4 {
             return;
         }
         if ((day.equals("monday")) || (day.equals("tuesday")) || (day.equals("wednesday")) || (day.equals("thursday")) || (day.equals("friday"))) {
-                        if (fruit.equals("banana")) {
-                            double count = kg * 2.50;
-                            System.out.println(String.format("%.2f", count));
-
-                        }
-                        else if (fruit.equals("apple")) {
-                            double count = kg * 1.20;
-                            System.out.println(String.format("%.2f", count));
-                        }
-                        else if (fruit.equals("orange")) {
-                            double count = kg * 0.85;
-                            System.out.println(String.format("%.2f", count));
-                        }
-                        else if (fruit.equals("grape fruit")) {
-                            double count = kg * 1.45;
-                            System.out.println(String.format("%.2f", count));
-                        }
-                        else if (fruit.equals("kiwi")) {
-                            double count = kg * 1.20;
-                            System.out.println(String.format("%.2f", count));
-                        }
-                        else if (fruit.equals("pineapple")) {
-                            double count = kg * 5.50;
-                            System.out.println(String.format("%.2f", count));
-                        }
-                        else if (fruit.equals("grapes")) {
-                            double count = kg * 3.85;
-                            System.out.println(String.format("%.2f", count));
-                        }
-                    }
-                }
-            if ((day.equals("saturday")) || (day.equals("sunday"))) {
-                    if ((fruit.equals("banana")) || (fruit.equals("apple")) || (fruit.equals("orange")) || (fruit.equals("grape fruit")) || (fruit.equals("kiwi")) || (fruit.equals("pineapple") || (fruit.equals("grapes")))) {
-                        if (fruit.equals("banana")) {
-                            double count = kg * 2.70;
-                            System.out.println(String.format("%.2f", count));
-
-                        }
-                        if (fruit.equals("apple")) {
-                            double count = kg * 1.25;
-                            System.out.println(String.format("%.2f", count));
-                        }
-                        if (fruit.equals("orange")) {
-                            double count = kg * 0.90;
-                            System.out.println(String.format("%.2f", count));
-                        }
-                        if (fruit.equals("grape fruit")) {
-                            double count = kg * 1.60;
-                            System.out.println(String.format("%.2f", count));
-                        }
-                        if (fruit.equals("kiwi")) {
-                            double count = kg * 3.00;
-                            System.out.println(String.format("%.2f", count));
-                        }
-                        if (fruit.equals("pineapple")) {
-                            double count = kg * 5.60;
-                            System.out.println(String.format("%.2f", count));
-                        }
-                        if (fruit.equals("grapes")) {
-                            double count = kg * 4.20;
-                            System.out.println(String.format("%.2f", count));
-                        }
-                    } else {
-                        System.out.println("error");
-                    }
-                }
-
-
+            if (fruit.equals("banana")) {
+                count = kg * 2.50;
             }
-        } else {
-            System.out.println("error");
+            else if (fruit.equals("apple")) {
+                count = kg * 1.20;  
+            }
+            else if (fruit.equals("orange")) {
+                count = kg * 0.85; 
+            }
+            else if (fruit.equals("grape fruit")) {
+                count = kg * 1.45;  
+            }
+            else if (fruit.equals("kiwi")) {
+                count = kg * 1.20;                
+            }
+            else if (fruit.equals("pineapple")) {
+                count = kg * 5.50;               
+            }
+            else if (fruit.equals("grapes")) {
+                count = kg * 3.85;               
+            }
         }
+        else if ((day.equals("saturday")) || (day.equals("sunday"))) {
+            if (fruit.equals("banana")) {
+                count = kg * 2.70;
+            }
+            else if (fruit.equals("apple")) {
+                count = kg * 1.25;                
+            }
+            else if (fruit.equals("orange")) {
+                count = kg * 0.90;                
+            }
+            else if (fruit.equals("grape fruit")) {
+                count = kg * 1.60;              
+            }
+            else if (fruit.equals("kiwi")) {
+                count = kg * 3.00;
+            }
+            else if (fruit.equals("pineapple")) {
+                count = kg * 5.60;    
+            }
+            else if (fruit.equals("grapes")) {
+                count = kg * 4.20;  
+            }
+        }
+        System.out.println(String.format("%.2f", count));
     }
 }
